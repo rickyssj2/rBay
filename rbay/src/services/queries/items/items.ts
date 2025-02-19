66440bad87bd6fb6c2709670e3db156bbdc22e92
Aryan Pandey <aryanpandey048@gmail.com>
2025-02-19 09:11:35 -0800

    Added getItems queury using pipelining

--- rbay/src/services/queries/items/items.ts
+++ rbay/src/services/queries/items/items.ts
@@ -15,7 +15,21 @@ export const getItem = async (id: string) => {
 	return deserialize(id, item);
 };
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]) => {
+	const commands = ids.map((id) => {
+		return client.hGetAll(itemsKey(id));
+	});
+
+	const results = await Promise.all(commands);
+
+	return results.map((item, i) => {
+		if (Object.keys(item).length === 0) {
+			return null;
+		}
+
+		return deserialize(ids[i], item);
+	});
+};
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const id = genId();
@@ -23,4 +37,6 @@ export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const serialized = serialize(attrs);
 
 	await client.hSet(itemsKey(id), serialized);
+
+	return id;
 };
