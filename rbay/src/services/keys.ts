20f91d1be1af50081a8c06c4ff4c472b5fa1acf8
Aryan Pandey <aryanpandey048@gmail.com>
2025-02-20 20:17:57 -0800

    Implemented getUserByUsername using Sorted Sets in Redis

--- rbay/src/services/keys.ts
+++ rbay/src/services/keys.ts
@@ -2,3 +2,6 @@ export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (id: string) => `users#${id}`;
 export const sessionKey = (id: string) => `session#${id}`;
 export const itemsKey = (id: string) => `items#${id}`;
+export const usernamesUniqueKey = () => `usernames:unique`;
+export const userLikesKey = (userId: string) => `users:likes#${userId}`;
+export const usernamesKey = () => `usernames`;
--- rbay/src/services/queries/users.ts
+++ rbay/src/services/queries/users.ts
@@ -1,9 +1,27 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey } from '$services/keys';
+import { usernamesKey, usernamesUniqueKey, usersKey } from '$services/keys';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+	// Use the username argument to look up the person's UserID
+	// in the `usernames` sorted set
+	const decimalId = await client.zScore(usernamesKey(), username);
+
+	// make sure we actually got an ID from the lookup
+	if (!decimalId) {
+		throw new Error(`User does not exist`);
+	}
+
+	// Take the ID and convert it back to Hexadecimal
+	const id = decimalId.toString(16);
+
+	// Use the ID to look up the user's hash
+	const user = await client.hGetAll(usersKey(id));
+
+	// Deserialize and return the has
+	return deserialize(id, user);
+};
 
 export const getUserById = async (id: string) => {
 	const user = await client.hGetAll(usersKey(id));
@@ -13,7 +31,23 @@ export const getUserById = async (id: string) => {
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
-	client.hSet(usersKey(id), serialize(attrs));
+
+	// See if username is already in use
+	const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username);
+
+	// if so throw an error
+	if (exists) {
+		throw new Error('Username is taken');
+	}
+
+	// Otherwise, continue
+	await client.hSet(usersKey(id), serialize(attrs));
+	await client.sAdd(usernamesUniqueKey(), attrs.username);
+	await client.zAdd(usernamesKey(), {
+		value: attrs.username,
+		score: parseInt(id, 16)
+	});
+
 	return id;
 };
 
